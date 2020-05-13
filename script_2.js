cf9339c9c98db4e8829649638f80fcd7ab76f6a6
jairomartinez1994 <61437470+jairomartinez1994@users.noreply.github.com>
2020-05-13 15:47:13 -0400

    Update script_2.js

--- script_2.js
+++ script_2.js
@@ -1,216 +1,136 @@
-var date = moment();
-console.log(date);
-var currentday = $("#currentDay");
-currentday.text(date);
+var currentDay = moment();
+var momentTime = moment('LT');
+
+console.log(moment());
+console.log(moment('12 10').isBefore(moment()));
+
+$("#currentDay").text(currentDay.format('LLLL'));
+
+var hours =
+	["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm",]
+
+var scheduler = $(".container");
+var save = $(".saveBtn");
+
+$.each(hours, function (i, time) {
+	scheduler.append('<div class="time-block"><div class="row"><div class="hour">' + hours[i] + '</div><textarea class="description" id="time' + hours[i] + '"></textarea><button class="saveBtn" id="save' + hours[i] + '"><i class="fa fa-bookmark"></i></button></div></div>');
+	console.log(time);
+});
+
+
+if ((new Date().getHours()) === 9){
+	$("#time9am").attr("class", "present");
+} else if ((new Date().getHours()) > 9){
+	$("#time9am").attr("class", "past");
+} else{
+	$("#time9am").attr("class", "future");
+};
+
+if ((new Date().getHours()) === 10){
+	$("#time10am").attr("class", "present");
+} else if ((new Date().getHours()) > 10){
+	$("#time10am").attr("class", "past");
+} else{
+	$("#time10am").attr("class", "future");
+};
+
+if ((new Date().getHours()) === 11){
+	$("#time11am").attr("class", "present");
+} else if ((new Date().getHours()) > 11){
+	$("#time11am").attr("class", "past");
+} else{
+	$("#time11am").attr("class", "future");
+};
+
+if ((new Date().getHours()) === 12){
+	$("#time12pm").attr("class", "present");
+} else if ((new Date().getHours()) > 12){
+	$("#time12pm").attr("class", "past");
+} else{
+	$("#time12pm").attr("class", "future");
+};
+
+if ((new Date().getHours()) === 13){
+	$("#time1pm").attr("class", "present");
+} else if ((new Date().getHours()) > 13){
+	$("#time1pm").attr("class", "past");
+} else{
+	$("#time1pm").attr("class", "future");
+};
+
+if ((new Date().getHours()) === 14){
+	$("#time2pm").attr("class", "present");
+} else if ((new Date().getHours()) > 14){
+	$("#time2pm").attr("class", "past");
+} else{
+	$("#time2pm").attr("class", "future");
+};
+
+if ((new Date().getHours()) === 15){
+	$("#time3pm").attr("class", "present");
+} else if ((new Date().getHours()) > 15){
+	$("#time3pm").attr("class", "past");
+} else{
+	$("#time3pm").attr("class", "future");
+};
+
+if ((new Date().getHours()) === 16){
+	$("#time4pm").attr("class", "present");
+} else if ((new Date().getHours()) > 16){
+	$("#time4pm").attr("class", "past");
+} else{
+	$("#time4pm").attr("class", "future");
+};
+
+if ((new Date().getHours()) === 17){
+	$("#time5pm").attr("class", "present");
+} else if ((new Date().getHours()) > 17){
+	$("#time5pm").attr("class", "past");
+} else{
+	$("#time5pm").attr("class", "future");
+};
+
+var input9am = document.querySelector("#time9am");
+var input10am = document.querySelector("#time10am");
+var input11am = document.querySelector("#time11am");
+var input12pm = document.querySelector("#time12pm");
+var input1pm = document.querySelector("#time1pm");
+var input2pm = document.querySelector("#time2pm");
+var input3pm = document.querySelector("#time3pm");
+var input4pm = document.querySelector("#time4pm");
+var input5pm = document.querySelector("#time5pm");
+
+var display9am = document.querySelector("#time9am");
+var display10am = document.querySelector("#time10am");
+var display11am = document.querySelector("#time11am");
+var display12pm = document.querySelector("#time12pm");
+var display1pm = document.querySelector("#time1pm");
+var display2pm = document.querySelector("#time2pm");
+var display3pm = document.querySelector("#time3pm");
+var display4pm = document.querySelector("#time4pm");
+var display5pm = document.querySelector("#time5pm");
+
+var saveButton = $(".saveBtn");
+
+saveButton.on("click", function(event) {
+	saveSchedule();
+});
+
+function saveSchedule(){
+	event.preventDefault();
+
+	var schedule = {
+	time9am: input9am.value.trim(),
+	time10am: input10am.value.trim(),
+	time11am: input11am.value.trim(),
+	time12pm: input12pm.value.trim(),
+	time1pm: input1pm.value.trim(),
+	time2pm: input2pm.value.trim(),
+	time3pm: input3pm.value.trim(),
+	time4pm: input4pm.value.trim(),
+	time5pm: input5pm.value.trim(),
+	};
+
+	localStorage.setItem("schedule", JSON.stringify(schedule));	
+};
 
-var workhours = document.querySelector("#work-hours");
-
-var workhours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
-
-
-renderWorkhours();
-function renderWorkhours() {
-    for (var i = 0; i < workhours; i++) {
-        var workhours = workhours[i];
-        var li = document.createElement("li");
-        li.setAttribute("data-index", i);
-        li.textContent = workhours;
-        workhours.appendChild(li);
-        
-        var button = document.createElement("button");
-        button.textContent = "save";
-        button.setAttribute("id", i + 9);
-        button.setAttribute("class", "save");
-
-        li.appendChild(button);
-
-
-        
-        var storageItem = localStorage.getItem(workhours[i]);
-        var inText = document.createElement("INPUT");
-        inText.setAttribute("type", "text");
-        inText.setAttribute("data-time", i + 9);
-        inText.setAttribute("class", "myText");
-        inText.id = (i + 9 + "t");
-        inText.value = storageItem;
-        
-        
-        console.log(storageItem);
-        li.appendChild(inText);
-
-
-
-
-
-    }
-
-}
-
-
-
-
-
-
-var saveBtn = document.querySelectorAll(".save");
-for (var i = 0; i < saveBtn.length; i++) {
-    saveBtn[i].addEventListener("click", function (event) {
-        event.preventDefault();
-
-        var elementId = this.getAttribute("id");
-        var x = '[data-time="' + elementId + '"]';
-        var inputDiv = document.querySelector(x);
-        var inputText = inputDiv.value;
-        console.log(inputText);
-    
-
-
-        localStorage.setItem(elementId, inputText);
-    
-    });
-}
-
-
-
-
-if ((new Date().getHours()) === 9) {
-
-    document.getElement('9t').style.backgroundColor = " #ff6961";
-
-} else if ((new Date().getHours()) > 9) {
-
-    document.getElementById('9t').style.backgroundColor = "#d3d3d3";
-} else {
-
-    document.getElementById('9t').style.backgroundColor = "#77dd77";
-
-}
-
-
-if ((new Date().getHours()) === 10) {
-
-
-    document.getElement('10t').style.backgroundColor = " #ff6961";
-
-} else if ((new Date().getHours()) > 10) {
-
-    document.getElementById('10t').style.backgroundColor = "#d3d3d3";
-
-} else {
-
-    document.getElementById('10t').style.backgroundColor = "#77dd77";
-
-
-}
-
-
-
-if ((new Date().getHours()) === 11) {
-
-
-    document.getElement('11t').style.backgroundColor = " #ff6961";
-
-} else if ((new Date().getHours()) > 11) {
-
-    document.getElementById('11t').style.backgroundColor = "#d3d3d3";
-
-} else {
-
-    document.getElementById('11t').style.backgroundColor = "#77dd77";
-
-}
-
-
-
-
-if ((new Date().getHours()) === 12) {
-
-
-    document.getElement('12t').style.backgroundColor = " #ff6961";
-
-} else if ((new Date().getHours()) > 12) {
-
-    document.getElementById('12t').style.backgroundColor = "#d3d3d3";
-
-} else {
-
-    document.getElementById('12t').style.backgroundColor = "#77dd77";
-
-}
-
-
-if ((new Date().getHours()) === 1) {
-
-
-    document.getElement('1t').style.backgroundColor = " #ff6961";
-
-} else if ((new Date().getHours()) > 1) {
-
-    document.getElementById('1t').style.backgroundColor = "#d3d3d3";
-
-} else {
-
-    document.getElementById('1t').style.backgroundColor = "#77dd77";
-}
-
-
-if ((new Date().getHours()) === 2) {
-
-
-    document.getElement('2t').style.backgroundColor = " #ff6961";
-
-} else if ((new Date().getHours()) > 2) {
-
-    document.getElementById('2t').style.backgroundColor = "#d3d3d3";
-
-} else {
-
-    document.getElementById('2t').style.backgroundColor = "#77dd77";
-}
-
-
-
-if ((new Date().getHours()) === 3) {
-
-
-    document.getElement('3t').style.backgroundColor = " #ff6961";
-
-} else if ((new Date().getHours()) > 3) {
-
-    document.getElementById('3t').style.backgroundColor = "#d3d3d3";
-
-} else {
-
-    document.getElementById('3t').style.backgroundColor = "#77dd77";
-}
-
-
-
-
-if ((new Date().getHours()) === 4) {
-
-
-    document.getElement('4t').style.backgroundColor = " #ff6961";
-
-} else if ((new Date().getHours()) > 4) {
-
-    document.getElementById('4t').style.backgroundColor = "#d3d3d3";
-
-} else {
-
-    document.getElementById('4t').style.backgroundColor = "#77dd77";
-}
-
-
-if ((new Date().getHours()) === 5) {
-
-
-    document.getElement('5t').style.backgroundColor = " #ff6961";
-
-} else if ((new Date().getHours()) > 5) {
-
-    document.getElementById('5t').style.backgroundColor = "#d3d3d3";
-
-} else {
-
-    document.getElementById('5t').style.backgroundColor = "#77dd77";
-}
